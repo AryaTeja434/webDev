8b9e112de785d57857f7261ef2856e1003ae6a48
AryaTeja434 <dsteja3@gmail.com>
2026-07-02 19:34:48 +0530

    http Server

--- Day47-httpServer/randomNumberServer.js
+++ Day47-httpServer/randomNumberServer.js
@@ -1,25 +1,41 @@
 
-const http=require("node:http")
+
+const http=require("http")
 const fs=require("node:fs")
 
-
-function logEvent(){
+function logEvent(ip){
+    console.log("Function called")
+    const timestamp = new Date().toISOString();
+    const message = `Time:${timestamp} Add:${ip}\n`
     
+    fs.appendFile("server.log",message,(err)=>{
+        if (err) console.log("Failed to log!")
+    })
 }
-const PORT = 8001
 
 let random=0
-
 setInterval(()=>{
-    random= Math.floor(Math.random()*100)
+    random = Math.floor(Math.random() * 100)
 },2000)
 
-const server = http.createServer((req,res)=>{
-    console.log("no")
-    res.write(`<h1>Random Number: ${random} </h1>`)
+
+const server= http.createServer((req,res)=>{
+    let ip=req.socket.remoteAddress
+    
+    logEvent(ip)
+    
+    res.write(`
+        <body>
+            <h1>Random Number Generator</h1>
+            <p class="number">Random Number: ${random}</p>
+            <p>This page automatically refreshes every 2 seconds.</p>
+        </body>
+        <script> setTimeout(()=>{location.reload()},2000) </script>
+        `)
     res.end()
 })
 
-server.listen(PORT,()=>{
-    console.log("server is loading")
+server.listen(8000,()=>{
+    console.log("Server loading")
 })
+
